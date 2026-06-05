62c007623ea8a4bf15a56f0e4fd8857451593d75
G Thimmaraju <thimmaraju418@gmail.com>
2026-06-05 09:36:16 +0530

    arrays

--- LearnJavaScript/arrays.js
+++ LearnJavaScript/arrays.js
@@ -1,2 +1,51 @@
+const { populate } = require("dotenv")
+
+arr  = []  // empty array 
+
+arr2 = [2,4,5,6,7,8]  // elements 
+
+arr3 = ["raju", 32, true , undefined, null]
+
+
+console.log(arr3[0])
+
+
+arr4 = [2,4,5,6,7,8,34,65,23,87]
+
+console.log(arr4.length)
+
+console.log(arr4[arr4.length-1])
+
+
+str1 = "Raju"
+str2 = "G"
+
+console.log(arr3.concat(arr4))
+
+arr5 = [2,7,9,3,5,6]
+
+//.sort()
+
+arr7 = ['raju', 'arun', "zion", "Parth"]
+
+arr6 = arr7.sort()
+
+console.log(arr6)
+
+//20+
+
+// push 
+// pop
+// shift 
+// unshift 
+// some 
+// any 
+// filter 
+// reduce 
+// map 
+// includes 
+// indexOf 
+//spilt
+//join
 
 
